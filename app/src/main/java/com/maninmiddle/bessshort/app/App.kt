8ab08c9dc8db825475058b43bb62ac74e0941439
maninmiddle <maninmiddler@gmail.com>
2024-06-18 04:06:35 +0300

    Initial commit. App layer

---
+++ app/src/main/java/com/maninmiddle/bessshort/app/App.kt
@@ -0,0 +1,17 @@
+package com.maninmiddle.bessshort.app
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.logger.Level
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@App)
+        }
+    }
+}
